40218ef3fbfe32096a06e32af0876e62ea3b3395
Gary <gear@thytruth.com>
2020-11-10 15:31:12 +0800

    update

--- execgroups/_ed_configs/vpc_tf/_chrootfiles/var/tmp/terraform/vpc.tf
+++ execgroups/_ed_configs/vpc_tf/_chrootfiles/var/tmp/terraform/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   aws_vpc_name    = var.vpc_name
   aws_region      = var.aws_default_region
   aws_azs         = var.availability_zones
-  az_count        = var.availability_zones_count
+  az_count        = tonumber(var.availability_zones_count)
   vpc_cidr_base   = var.subnet_base
 
   global_tags = {
