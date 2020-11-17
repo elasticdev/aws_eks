fd75f9bcae03dc49c12cfa3ffec68c1e228d2458
Gary <gary@elasticdev.io>
2020-11-17 21:34:09 +0800

    update

---
+++ stacks/_ed_configs/eks_cluster/_documentation/README.md
@@ -0,0 +1,15 @@
+**Description**
+  - The stack creates EKS in a provider VPC already in resource table
+
+**Required**
+
+vpc_name
+eks_cluster
+
+**Optional**
+
+```
+aws_default_region: us-east-1
+availability_zones - list converted to string
+subnet_base - string
+```
---
+++ stacks/_ed_configs/eks_cluster/_main/run.py
@@ -0,0 +1,110 @@
+def _get_subnet_ids(vpc_info,subnet_names=None):
+
+    if not subnet_names:
+        subnet_names = ["private_prod","private"]
+
+    data = vpc_info["raw"]["terraform"]
+
+    subnet_ids = []
+
+    for resource in data["resources"]:
+        for instance in resource["instances"]:
+            _type = resource["type"]
+            _name = resource["name"]
+            _mode = resource.get("mode")
+    
+            # this is the main category for the terraform template
+            if _type != "aws_subnet": continue
+            if _mode != "managed": continue
+            if _name not in subnet_names: continue
+    
+            _results = instance["attributes"]
+            subnet_ids.append(_results["id"])
+
+    if not subnet_ids: return 
+    
+    return ",".join(subnet_ids)
+
+def run(stackargs):
+
+    import json
+
+    # instantiate authoring stack
+    stack = newStack(stackargs)
+
+    # Add default variables
+    stack.parse.add_required(key="vpc_name")
+    stack.parse.add_required(key="eks_cluster")
+
+    # docker image to execute terraform with
+    stack.parse.add_optional(key="docker_exec_env",default="elasticdev/terraform-run-env")
+
+    stack.parse.add_optional(key="aws_default_region",default="us-west-1")
+    stack.parse.add_optional(key="eks_min_capacity",default="1")
+    stack.parse.add_optional(key="eks_max_capacity",default="2")
+    stack.parse.add_optional(key="eks_desired_capacity",default="1")
+    stack.parse.add_optional(key="resource_type",default="eks")
+    stack.parse.add_optional(key="subnet_names",default="null")
+
+    # Add execgroup
+    stack.add_execgroup("elasticdev:::aws_eks::eks_tf")
+
+    # Initialize 
+    stack.init_variables()
+
+    # Initialize execgroups
+    stack.init_execgroups()
+
+    vpc_info = stack.get_resource(name=stack.vpc_name,
+                                  resource_type="vpc",
+                                  region=stack.aws_default_region,
+                                  must_exists=True)[0]
+
+    # Execute execgroup for creating vpc
+    stateful_id = stack.random_id()
+
+    env_vars = {"NAME":stack.eks_cluster}
+    env_vars["K8_NAME"] = stack.eks_cluster
+    env_vars["EKS_NAME"] = stack.eks_cluster
+    env_vars["EKS_CLUSTER"] = stack.eks_cluster
+    env_vars["CLOBBER"] = True
+    env_vars["VPC_NAME"] = stack.vpc_name
+    env_vars["STATEFUL_ID"] = stateful_id
+    env_vars["METHOD"] = "create"
+
+    env_vars["TF_VAR_vpc_id"] = vpc_info["vpc_id"]
+    env_vars["TF_VAR_subnet_ids"] = _get_subnet_ids(vpc_info,
+                                                    subnet_names=stack.subnet_names)
+
+    if env_vars["TF_VAR_subnet_ids"] in ["None",None]: 
+        msg = "subnet_id are not found!"
+        stack.logger.error(msg)
+        stack.ehandle.NeedMoreInfo(message=msg)
+
+    # zones need to include a , and a space
+    env_vars["TF_VAR_eks_min_capacity"] = stack.eks_min_capacity
+    env_vars["TF_VAR_eks_max_capacity"] = stack.eks_max_capacity
+    env_vars["TF_VAR_eks_desired_capacity"] = stack.eks_desired_capacity
+    env_vars["TF_VAR_eks_cluster"] = stack.eks_cluster
+    env_vars["TF_VAR_aws_default_region"] = stack.aws_default_region
+
+    env_vars["RESOURCE_TYPE"] = stack.resource_type
+    env_vars["RESOURCE_TAGS"] = "{},{},{},{},{},{}".format("vpc","eks", "aws_eks", stack.eks_cluster, stack.vpc_name, stack.aws_default_region)
+    env_vars["USE_DOCKER"] = True
+    env_vars["DOCKER_EXEC_ENV"] = stack.docker_exec_env
+
+    _docker_env_fields_keys = env_vars.keys()
+    _docker_env_fields_keys.append("AWS_ACCESS_KEY_ID")
+    _docker_env_fields_keys.append("AWS_SECRET_ACCESS_KEY")
+    _docker_env_fields_keys.append("AWS_DEFAULT_REGION")
+    _docker_env_fields_keys.remove("METHOD")
+
+    env_vars["DOCKER_ENV_FIELDS"] = ",".join(_docker_env_fields_keys)
+
+    inputargs = {"display":True}
+    inputargs["env_vars"] = json.dumps(env_vars)
+    inputargs["name"] = stack.vpc_name
+    inputargs["stateful_id"] = stateful_id
+    stack.eks_tf.insert(**inputargs)
+
+    return stack.get_results()
